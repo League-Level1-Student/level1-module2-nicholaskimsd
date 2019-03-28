860def38444a8fd924f6a11fda48426f4d4879b3
League <league@WTS8.attlocal.net>
2019-03-27 19:48:09 -0700

    r

--- src/sea_creature/SeaCreatur.java
+++ src/sea_creature/SeaCreatur.java
@@ -3,12 +3,6 @@ package sea_creature;
 public class SeaCreatur {
 	public static void main(String[] args) {
 		SeaCreatur bob = new SeaCreatur();
-		try {
-			bob.finalize();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 	}
 

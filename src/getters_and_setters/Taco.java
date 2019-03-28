27e8df4b6d4ade66008625cf8b7aebe7f9221a3a
League <league@WTS8.attlocal.net>
2019-03-27 19:47:09 -0700

    i am working on the intro and sea creature both are in IP

---
+++ src/getters_and_setters/Taco.java
@@ -0,0 +1,23 @@
+package getters_and_setters;
+
+public class Taco {
+public static void main(String[] args) {
+	setSauce();
+	setMeat();
+}
+public String getSauce() {
+return getSauce();
+}
+public String getmeat() {
+	return getmeat();
+	
+}
+public static String setMeat() {
+	return setMeat();
+	
+}
+public static String setSauce() {
+	return setSauce();
+	
+}
+}
